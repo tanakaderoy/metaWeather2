62b811c5b56b92ed19c496df5e1c5717f75ffc3a
tanakaderoy <tanaka.deroy@gmail.com>
2020-06-29 11:23:26 -0400

    Initial Commit

---
+++ MetaWeather2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MetaWeather2
+//
+//  Created by Tanaka Mazivanhanga on 6/29/20.
+//  Copyright © 2020 Tanaka Mazivanhanga. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
